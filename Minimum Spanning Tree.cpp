e886909d7bfb35fe4051b8e4cdb5e562187e9bfd
Ramkumardehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2023-07-06 22:42:39 +0530

    Create Minimum Spanning Tree.cpp

---
+++ Minimum Spanning Tree.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        
+        vector<int> vis(V, 0);
+        
+        pq.push({0,0});
+        int sum = 0;
+        
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            
+            int wt = it.first;
+            int node = it.second;
+            
+            if(vis[node] == 1){
+                continue;
+            }
+            vis[node] = 1;
+            sum += wt;
+            
+            for(auto it1 : adj[node]){
+                int adjNode = it1[0];
+                int ewt = it1[1];
+                
+                if(!vis[adjNode]){
+                    pq.push({ewt, adjNode});
+                }
+            }
+        }
+        return sum;
+    }
+};
